6029306ef0e451f2270b1833e3bc4737352cb2cc
Emma <seoyoon7011@gmail.com>
2017-12-19 17:01:15 +1100

    Tuesday

---
+++ Emma/wk04/2-tue/collection.rb
@@ -0,0 +1,5 @@
+planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
+#Access the second value in planeteers.
+# puts planeteers[1]
+#Add "Heart" to the end of planeteers.
+puts planeteers.push("Heart")
