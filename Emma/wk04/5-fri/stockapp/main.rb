ea460baef98731627cc8e77b290cf2d0dd2e2b51
Emma <seoyoon7011@gmail.com>
2018-01-22 22:44:39 +1100

    stock app

---
+++ Emma/wk04/5-fri/stockapp/main.rb
@@ -0,0 +1,17 @@
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'stock_quote'
+# require 'pry'
+
+get '/' do
+  @stock = StockQuote::Stock.quote(params[:stock])
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+# binding.pry
+# puts "paused"
