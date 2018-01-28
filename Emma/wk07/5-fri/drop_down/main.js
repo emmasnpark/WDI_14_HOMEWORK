dd813a1e9d46cc0fe902ad1197171274a04f3bba
Emma <seoyoon7011@gmail.com>
2018-01-28 22:58:18 +1100

    drop down

---
+++ Emma/wk07/5-fri/drop_down/main.js
@@ -0,0 +1,30 @@
+console.log("hi");
+var myobject = {
+    nyc : 'NYC',
+    sf : 'SF',
+    la : 'LA',
+    sydney : 'SYD',
+    austin : 'ATX'
+};
+
+var select = document.getElementById("city-type");
+for(property in myobject) {
+    select.options[select.options.length] = new Option(myobject[property], property);
+}
+
+var navigationSelect = document.getElementById("city-type");
+
+//Navigate to URL when select box is changed
+var navigateToValue = function() {
+  var element = document.querySelector("body")
+  element.classList.remove("nyc","sf","la","sydney","austin");
+  element.classList.add(this.value);
+  console.log(this.value);
+}
+
+//Send analytics data
+// var sendAnalytics = function() {
+//   alert("hi")
+// }
+navigationSelect.addEventListener('change',  navigateToValue);
+//navigationSelect.addEventListener('change',  sendAnalytics);
