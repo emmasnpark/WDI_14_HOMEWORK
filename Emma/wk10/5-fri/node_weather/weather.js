88feb7e2b7221afaa620150917a42d197aea8774
Emma <seoyoon7011@gmail.com>
2018-02-19 10:06:43 +1100

    weather

--- Emma/wk10/5-fri/node_weather/weather.js
+++ Emma/wk10/5-fri/node_weather/weather.js
@@ -1,19 +1,14 @@
-var http = require('http')
-function app(request, response) {
-  var path =  request.url
-  if(path === '/') {
-    console.log("welcome");
-    response.write('welcome');
-    response.end();
-  }else {
-    console.log(path);
-    response.write(path);
-    response.end();
-  }
 
-}
-var server = http.createServer(app) //create a createServer
+var request = require('request')
+var city = process.argv[2]
 
-server.listen(8080, function() {
-  console.log("listening");
-})
+var apikey = "bc6cb2e7e1e88f304611203162b9a424"
+var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apikey
+
+request(url, function (error, response, body) {
+  url: url
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.dir(JSON.parse(body).main.temp);
+   // Print the HTML for the Google homepage.
+});
